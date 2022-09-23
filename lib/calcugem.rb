71ff6cb71f0f30713fbaee162690d1acc85c3877
Hansel López <halm29799@gmail.com>
2022-09-23 05:49:24 -0600

    create basic calculator Calcugem

---
+++ lib/calcugem.rb
@@ -0,0 +1,22 @@
+# Hansel Lopez
+# 23/09/2022
+
+# Basic calculator ruby gem
+
+class Calcugem
+  def add(a, b)
+    a + b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    a / b
+  end
+end
